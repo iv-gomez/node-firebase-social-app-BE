394e27220df89868ee49a8588f2a8ed0da79dbf7
ivgomez <iv.gomez@hotmail.com>
2020-11-03 00:03:58 -0500

    add notifications

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const app = require("express")();
-
+const { db } = require("./util/admin");
 const FBAuth = require("./util/fbAuth");
 
 const {
@@ -38,3 +38,72 @@ app.get("/user", FBAuth, getAuthenticatedUser);
 
 // https://baseUrl.com/api/
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  .region("us-west3")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.log(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationsOnUnlike = functions
+  .region("us-west3")
+  .firestore.document("likes/{id}")
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.log(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions
+  .region("us-west3")
+  .firestore.document("comments/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.log(err);
+        return;
+      });
+  });
